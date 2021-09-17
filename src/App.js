bc4c92dbbc77254576d6ad95a11c48ca08cdc05d
dbaldew <dbaldew@gmail.com>
2021-09-17 16:22:51 +0200

    basic html elements & css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,39 @@ import './App.css';
 function App() {
     return (
         <>
-            <div className="container">
+            <div className="box">
+
+                <form className="form">
+
+                    <label className="formLabel" htmlFor="voornaam">
+                        Voornaam<input className="formInput" type="text" name="voornaam"/>
+                    </label>
+
+                    <label className="formDropDownLabel" htmlFor="delivFreq">Bezorgfrequentie
+                        <select
+                            className="fromDropDown" name="delivFreq" id="delivFreq">
+                            <option value="iedere week">Iedere week</option>
+                            <option value="om de week">Om de week</option>
+                            <option value="iedere week">Iedere maand</option>
+                        </select>
+                    </label>
+
+                    <label className="delivTimeRadioLabel"htmlFor="delivTimeRadio">
+                        <input className="delivTimeRadio" type="radio" name="delivTimeRadio"/>Overdag
+                    </label>
+
+                    <label className="commentLabel" htmlFor="comment">Opmerking</label>
+                    <textarea className="comment" name="comment" id="comment" cols="20" rows="7" fixed = "true"></textarea>
+
+                    <label className="agreeLabel" htmlFor="agreeChk">
+                        <input className="agreeCheckbox" type="checkbox" name="agreeChk"/>Ik ga akkoord met de voorwaarden
+                    </label>
+
+                    <label htmlFor="submitBtn">
+                        <button className="submitBtn" type="submit" name="submitBtn">Verzend</button>
+                    </label>
+
+                </form>
 
             </div>
 
